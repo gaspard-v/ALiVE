cf0f1913d109fec53e20618b6da323d289205d76
Gaspard VANDECASTEELE <gaspard.vandecasteele@gmail.com>
2022-09-20 12:57:01 +0200

    add some data

--- src/manifest.js
+++ src/manifest.js
@@ -1,18 +1,23 @@
 // a melonJS data manifest
 // note : this is note a webpack manifest
 const DataManifest = [
-
     /* Bitmap Text */
     {
-        name: "PressStart2P",
-        type: "image",
-        src:  "./data/fnt/PressStart2P.png"
+        name: 'PressStart2P',
+        type: 'image',
+        src: './data/fnt/PressStart2P.png',
     },
     {
-        name: "PressStart2P",
-        type: "binary",
-        src: "./data/fnt/PressStart2P.fnt"
-    }
+        name: 'PressStart2P',
+        type: 'binary',
+        src: './data/fnt/PressStart2P.fnt',
+    },
+    {
+        name: 'area01_level_tiles',
+        type: 'image',
+        src: 'data/img/map/area01_level_tiles.png',
+    },
+    { name: 'area01', type: 'tmx', src: 'data/map/area01.tmx' },
 ];
 
 export default DataManifest;
