09713b3f510bf34f2208336eb3903f4c2439034e
thomas-fichten <80630940+thomas-fichten@users.noreply.github.com>
2022-09-27 10:36:43 +0200

    feat: make an image clickable

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -7,9 +7,23 @@ export default class PlayScene extends Phaser.Scene {
 
     preload() {
         this.load.image('luxmetre', '/static/luxmetrePro.jfif');
+        this.load.text("luxmetreText", "/static/luxmetreText.txt");
+    }
+
+    showText(value) {
+        console.log("I would to show a text.")
+        this.add.text(game.config.width / 2, game.config.height / 2, "luxmetreText").setVisible(value);
+        console.log("End of showText method.")
+    }
+
+    reverseText() {
+        let val = this.add.text(game.config.width / 2, game.config.height / 2, "luxmetreText").visible;
+        console.log("luxmetreText is visible : " + val)
+        this.showText(!val);
     }
 
     create() {
-        this.add.image(game.config.width / 2, game.config.height / 2, 'luxmetre');
+        this.add.image(game.config.width / 2, game.config.height / 2, 'luxmetre')
+            .setInteractive({ useHandCursor: true }).on('pointerdown', () => this.reverseText());
     }
 }
