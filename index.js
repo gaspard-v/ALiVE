97b0abc5fc833fb902cf64751e17185e1b76dc29
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-09-27 09:33:21 +0200

    add css and ration
    ration is automatic

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ import * as Phaser from 'phaser';
 import PlayScene from './src/scenes/PlayScene';
 import MainMenuScene from './src/scenes/MainMenu';
 
+const ratio = Math.max(
+    window.innerWidth / window.innerHeight,
+    window.innerHeight / window.innerWidth
+);
+const DEFAULT_HEIGHT = 720; // any height you want
+const DEFAULT_WIDTH = ratio * DEFAULT_HEIGHT;
+
 const config = {
     name: 'ALiVE',
     type: Phaser.AUTO,
     backgroundColor: '#282828',
     scale: {
         mode: Phaser.Scale.FIT,
-        width: 800,
-        height: 600,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: DEFAULT_WIDTH,
+        height: DEFAULT_HEIGHT,
     },
     autoCenter: true,
     antialias: true,
