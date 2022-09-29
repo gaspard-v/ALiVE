50c2b7569920bbeb4701af8783e1cb7a26891cee
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-09-29 10:10:12 +0200

    modification playscene

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -7,23 +7,39 @@ export default class PlayScene extends Phaser.Scene {
 
     preload() {
         this.load.image('luxmetre', '/static/luxmetrePro.jfif');
-        this.load.text("luxmetreText", "/static/luxmetreText.txt");
+        this.load.text('luxmetreText', '/static/luxmetreText.txt');
     }
 
     showText(value) {
-        console.log("I would to show a text.")
-        this.add.text(game.config.width / 2, game.config.height / 2, "luxmetreText").setVisible(value);
-        console.log("End of showText method.")
+        console.log('I would to show a text.');
+        this.add
+            .text(game.config.width / 2, game.config.height / 2, 'luxmetreText')
+            .setVisible(value);
+        console.log('End of showText method.');
     }
 
-    reverseText() {
-        let val = this.add.text(game.config.width / 2, game.config.height / 2, "luxmetreText").visible;
-        console.log("luxmetreText is visible : " + val)
-        this.showText(!val);
+    reverseText(texte) {
+        // let val = this.add.text(
+        //     game.config.width / 2,
+        //     game.config.height / 2,
+        //     'luxmetreText'
+        // ).visible;
+        // console.log('luxmetreText is visible : ' + val);
+        // this.showText(!val);
+        //texte.bringToTop();
+        texte.setVisible(!texte.visible);
     }
 
     create() {
-        this.add.image(game.config.width / 2, game.config.height / 2, 'luxmetre')
-            .setInteractive({ useHandCursor: true }).on('pointerdown', () => this.reverseText());
+        const texte = this.add
+            .text(0, 0, 'luxmetreText', {
+                fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
+            })
+            .setVisible(false);
+        const luxmeter = this.add
+            .image(game.config.width / 2, game.config.height / 2, 'luxmetre')
+            .setInteractive({ useHandCursor: true });
+
+        luxmeter.on('pointerup', () => this.reverseText(texte));
     }
 }
