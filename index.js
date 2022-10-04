1787537a774df4b284f3139ebb37bd469ee86d40
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-10-04 08:20:47 +0200

    TYPO modification erreur format affichage
    "width" et "height" étais inversé

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const config = {
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        width: 1080,
-        height: 1920,
+        width: 1920,
+        height: 1080,
     },
     autoCenter: true,
     antialias: true,
