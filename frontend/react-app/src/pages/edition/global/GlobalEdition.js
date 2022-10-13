30467f4186c595f0ea6b319c340bf5fa1b521f67
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-13 11:41:55 +0200

    Init Edition Page

---
+++ frontend/react-app/src/pages/edition/global/GlobalEdition.js
@@ -0,0 +1,14 @@
+import "./global.css";
+import React from "react";
+
+function GlobalEdition(){
+    return(
+        <>
+        <div className="fix">
+            <h1>Edition</h1>
+        </div>
+        </>
+    );
+}
+
+export default GlobalEdition;
