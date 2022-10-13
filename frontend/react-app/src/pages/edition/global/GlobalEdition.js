f67a6bddc063e66f3c42958b02e3aa55374dc237
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-13 17:00:52 +0200

    SearchBar init

--- frontend/react-app/src/pages/edition/global/GlobalEdition.js
+++ frontend/react-app/src/pages/edition/global/GlobalEdition.js
@@ -1,11 +1,13 @@
 import "./global.css";
 import React from "react";
+import SearchBar from "../../../components/searchbar/SearchBar";
 
 function GlobalEdition(){
     return(
         <>
         <div id="globalEdition">
             Edition
+            <SearchBar/>
         </div>
         </>
     );
