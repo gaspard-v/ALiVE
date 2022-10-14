951f6d64d7fcb504c5f6931a6a755221c87bb864
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-14 16:43:35 +0200

    Global edition

--- frontend/react-app/src/pages/edition/global/GlobalEdition.js
+++ frontend/react-app/src/pages/edition/global/GlobalEdition.js
@@ -1,13 +1,23 @@
 import "./global.css";
 import React from "react";
 import SearchBar from "../../../components/searchbar/SearchBar";
+import days from "../../../ressources/img/component/button/Days.jpg";
+import rooms from "../../../ressources/img/component/button/Rooms.jpg";
+import maps from "../../../ressources/img/component/button/Maps.jpg";
+import objects from "../../../ressources/img/component/button/Object.jpg";
 
 function GlobalEdition(){
     return(
         <>
         <div id="globalEdition">
             Edition
-            <SearchBar/>
+            <SearchBar/> 
+            <div className="Buttons">
+            <button id="Objects" className="EditButton"><img src={objects} alt="Objets"></img></button>
+            <button id="Rooms" className="EditButton"><img src={rooms} alt="Rooms"></img></button>
+            <button id="Maps" className="EditButton"><img src={maps} alt="Maps"></img></button>
+            <button id="Days" className="EditButton"><img src={days} alt="Days"></img></button>
+            </div>   
         </div>
         </>
     );
