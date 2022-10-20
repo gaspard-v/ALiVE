dec1b65ba37cb7ab15326c1fc2d5c445ec512fb4
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-20 13:26:54 +0200

    Routes

---
+++ frontend/react-app/src/pages/edition/days/DaysEdition.js
@@ -0,0 +1,13 @@
+import "./days.css";
+import React from "react";
+
+function DaysEdition(){
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default DaysEdition;
---
+++ frontend/react-app/src/pages/edition/objects/ObjectsEdition.js
@@ -0,0 +1,13 @@
+import "./objects.css";
+import React from "react";
+
+function ObjectsEdition(){
+    return(
+        <>
+        
+        </>
+    )
+
+}
+
+export default ObjectsEdition;
