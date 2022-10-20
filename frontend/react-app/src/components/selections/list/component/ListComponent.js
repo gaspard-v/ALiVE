aee8795a8beb5af9c0700fbba8e9a1d51b63c8b2
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-20 14:50:18 +0200

    Wip ObjectPage

---
+++ frontend/react-app/src/components/selections/list/component/ListComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ListComponent(){
+
+    return(
+        <div id="ListComponent">
+            <img></img>
+            <p className="objectName">Name</p>
+        </div>
+    )
+
+}
+
+export default ListComponent;
--- frontend/react-app/src/pages/edition/objects/ObjectsEdition.js
+++ frontend/react-app/src/pages/edition/objects/ObjectsEdition.js
@@ -1,10 +1,15 @@
 import "./objects.css";
 import React from "react";
+import SearchBar from "../../../components/searchbar/SearchBar";
+import ListComponent from "../../../components/selections/list/component/ListComponent";
 
 function ObjectsEdition(){
     return(
         <>
-        
+        <div id="objectEdition">
+            <SearchBar/>
+            <ListComponent/>
+        </div>
         </>
     )
 
