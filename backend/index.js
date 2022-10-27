9de77876dfd65ef4392190edbfcef9d3b343b9b0
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2022-10-27 12:43:11 +0200

    add json body

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+app.use(express.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api', (req, res) => {
-    res.send('ALiVE api is running.');
+app.get("/api", (req, res) => {
+  res.send("ALiVE api is running.");
 });
 
-app.get('/api/object/1', function(req, res, next){
-    res.render('index'), {title: 'cool', condition: true, array: [1, 2, 3]};
+app.get("/api/object/:id", function (req, res, next) {
+  res.json({ output: req.params.id });
 });
 
-app.post('/api/objet/create', function(req, res) {
-    console.log('receiving data ...');
-    console.log('body is ',req.body);
-    res.send(req.body);
+app.post("/api/object/create", function (req, res) {
+  console.log("receiving data ...");
+  console.log("body is ", req.body);
+  res.send(req.body);
 });
 
-app.listen(8080, () => console.log('ALiVE app server is listening on port 8080.'));
+app.listen(8080, () =>
+  console.log("ALiVE app server is listening on port 8080.")
+);
