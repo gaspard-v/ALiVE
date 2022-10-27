36814ad81a3dedaf61352b6de71805b30296e255
thomas-fichten <80630940+thomas-fichten@users.noreply.github.com>
2022-10-27 11:10:55 +0200

    feat: change post root to create an object

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,10 @@ app.get('/api', (req, res) => {
     res.send('ALiVE api is running.');
 });
 
-app.post('/api/objet', function(req, res) {
+app.post('/api/objet/create', function(req, res) {
     console.log('receiving data ...');
     console.log('body is ',req.body);
     res.send(req.body);
 });
 
-app.listen(3000, () => console.log('ALiVE app server is listening on port 3000.'));
+app.listen(8080, () => console.log('ALiVE app server is listening on port 8080.'));
