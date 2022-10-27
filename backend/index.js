77eddcf810a972d08ee7c32fcd2942d7446ee425
thomas-fichten <80630940+thomas-fichten@users.noreply.github.com>
2022-10-27 11:21:36 +0200

    feat: get objet static

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ app.get('/api', (req, res) => {
     res.send('ALiVE api is running.');
 });
 
+app.get('/api/object/1', function(req, res, next){
+    res.render('index'), {title: 'cool', condition: true, array: [1, 2, 3]};
+});
+
 app.post('/api/objet/create', function(req, res) {
     console.log('receiving data ...');
     console.log('body is ',req.body);
