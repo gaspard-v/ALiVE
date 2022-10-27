193581669b858d15f3ca43d50bc6f10e26aa94f8
thomas-fichten <80630940+thomas-fichten@users.noreply.github.com>
2022-10-27 10:48:53 +0200

    feat: try object put into back

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 const http = require('http');
+const https = require('https');
 
 const server = http.createServer(function (req, res) {
     res.statusCode = 200; // 200 = OK
     res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>Demo page</h1>");
+    res.write("<h1>ALiVE Demo back</h1>");
+
+    const parsedUrl = url.parse(req.url, true);
+
+    if (parsedUrl.pathname === '/api/objet' && req.method === 'PUT') {
+        var ipAddr = extractIPAddress(parsedUrl.pathname);
+
+        if(!ipAddr.length) {
+            res.statusCode = 400;
+            res.end("Invalid IP Address");
+        } else {
+            handleUpdate(ipAddr,res);
+        }
+    }
+
     res.end();
 });
 
