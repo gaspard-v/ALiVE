014a7929498874a04d83cde80ca7869d60bb8324
thomas-fichten <80630940+thomas-fichten@users.noreply.github.com>
2022-10-27 11:04:40 +0200

    refactor: use express for CRUD

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,15 @@
-const http = require('http');
-const https = require('https');
+const express = require('express');
 
-const server = http.createServer(function (req, res) {
-    res.statusCode = 200; // 200 = OK
-    res.setHeader('Content-Type', 'text/html');
-    res.write("<h1>ALiVE Demo back</h1>");
+const app = express();
 
-    const parsedUrl = url.parse(req.url, true);
-
-    if (parsedUrl.pathname === '/api/objet' && req.method === 'PUT') {
-        var ipAddr = extractIPAddress(parsedUrl.pathname);
-
-        if(!ipAddr.length) {
-            res.statusCode = 400;
-            res.end("Invalid IP Address");
-        } else {
-            handleUpdate(ipAddr,res);
-        }
-    }
-
-    res.end();
+app.get('/api', (req, res) => {
+    res.send('ALiVE api is running.');
 });
 
-server.listen(3000, function () {
-    console.log("Listening on port http://localhost:3000");
+app.post('/api/objet', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(req.body);
 });
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
