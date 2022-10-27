bf076d4a88fe39435b137160b92d8edee87bc6be
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2022-10-27 08:45:11 +0200

    Edit Navigation

--- frontend/react-app/src/components/selections/edition/buttons/Buttons.jsx
+++ frontend/react-app/src/components/selections/edition/buttons/Buttons.jsx
@@ -1,16 +1,15 @@
 import './buttons.css';
 import React from "react";
+import { useState,useEffect } from 'react';
+import ObjectCreation from '../objects/creation/ObjectCreation';
 
-const Buttons = ({icon, children, type}) => {
 
 
+const Buttons = ({icon, children, type, trigger}) => {
 
-    const addObject = event => { 
-
-    }
 
     return(
-        <button className={`ModifyObject ${type}`} onClick={type}>
+        <button className={`ModifyObject ${type}`} onClick={trigger}>
             {icon}
             <p  className='ModifyObjectText'>{children}</p>
         </button>
--- frontend/react-app/src/pages/edition/objects/ObjectsEdition.jsx
+++ frontend/react-app/src/pages/edition/objects/ObjectsEdition.jsx
@@ -1,16 +1,22 @@
 import "./objects.css";
 import React from "react";
+import { useState,useEffect } from "react";
 import ListMenu from "../../../components/selections/list/menu/ListMenu";
 import ObjectPrompt from "../../../components/selections/edition/objects/prompt/ObjectPrompt"
 import ObjectCreation from "../../../components/selections/edition/objects/creation/ObjectCreation";
 
 function ObjectsEdition(){
 
+    const[currentSection,setCurrentSection] = useState(<ObjectPrompt/>)
+
+
+
+
+
     return(
         <div id="objectEdition">
-            <ListMenu/>
-            <ObjectCreation/>
-            {/* <ObjectPrompt/> */}
+            <ListMenu sectionChanger={setCurrentSection}/>
+            {currentSection}
         </div>
     )
 
