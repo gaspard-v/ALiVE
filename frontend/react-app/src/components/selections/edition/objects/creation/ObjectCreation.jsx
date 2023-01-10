5c59beb295b8cba8446d764fa4fc205a1274018c
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2023-01-10 10:26:10 +0100

    Fix: react-app

--- frontend/react-app/src/components/selections/edition/objects/creation/ObjectCreation.jsx
+++ frontend/react-app/src/components/selections/edition/objects/creation/ObjectCreation.jsx
@@ -5,7 +5,7 @@ import Buttons from "../../buttons/Buttons";
 import { TiTick } from "react-icons/ti"
 import SegmentedControl from "../../../../segmented/SegmentedControl";
 import { FiUpload } from "react-icons/fi"
-import createObject from "../../../../../api/object/ObjectCall";
+import {createObject} from "../../../../../api/object/ObjectCall";
 
 
 function ObjectCreation(){
@@ -29,23 +29,6 @@ function ObjectCreation(){
         <form id="ObjectEditionScreen" className="creation" onSubmit={onSubmit}>
             <label className="objectLabel">Nom de l'objet</label>
             <input className="ObjectName" value={objectName} onChange={e => setObjectName(e.target.value)}></input>
-            <SegmentedControl
-                    name="group-1"
-                    callback={(val) => setSelectedValue1(val)}
-                    controlRef={segmentedControl}
-                    segments={[
-                    {
-                        label: "Objet",
-                        value: "object",
-                        ref: useRef()
-                    },
-                    {
-                        label: "Intel",
-                        value: "intel",
-                        ref: useRef()
-                    },
-                ]
-            }/>
             <label className="descriptionLabel">Description de l'objet</label>
             <input className="ObjectDescription" value={objectDescription} onChange={e => setObjectDescription(e.target.value)}></input>
             <input className="ObjImg" type="file" name="img" accept="image/*" value={objectImg} onChange={e => setObjectImg(e.target.value)}></input>
