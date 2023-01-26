2d273097a248ce4391175ebc39e39121226ce134
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-26 14:40:42 +0100

    Ajout GET objet day
    ajout d'un GET pour récupérer les day, selon un UUID (ou pas)

---
+++ backend/day.js
@@ -0,0 +1,43 @@
+import { handlerError, handlerSuccess } from "./handler.js";
+
+export function getDay(pool, day_uuid = "", full = false) {
+  let conn;
+  let query = `SELECT HEX(Day.uuid) as uuid, Day.name as name, Day.description as description `;
+  let parameters = [];
+  if (day_uuid) {
+    query += ` WHERE Day.uuid = UNHEX(?) `;
+    parameters.append(day_uuid);
+  }
+  return pool
+    .getConnection()
+    .then((connexion) => {
+      conn = connexion;
+      return conn.query(query, parameters);
+    })
+    .then((result) => result)
+    .finally(() => {
+      if (conn) conn.end();
+    });
+}
+
+export const Day = (app, pool) => {
+  app.get("/api/day", async function (req, res, next) {
+    getDay(pool)
+      .then((result) => {
+        handlerSuccess(result, req, res, next);
+      })
+      .catch((err) => {
+        handlerError(err, req, res, next);
+      });
+  });
+  app.get("/api/day/:uuid", async function (req, res, next) {
+    const uuid = req.params.uuid;
+    getDay(pool, uuid)
+      .then((result) => {
+        handlerSuccess(result, req, res, next);
+      })
+      .catch((err) => {
+        handlerError(err, req, res, next);
+      });
+  });
+};
