ac218cab89df10250dc4d83503411f6e3461a842
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-01-31 11:25:20 +0100

    ajout GET File sur tout les élement jointés

--- backend/file.js
+++ backend/file.js
@@ -1,43 +1,48 @@
 import { handlerError, handlerSuccess } from "./handler.js";
 
-export function getFile(pool, file_uuid = "", other_uuid = "") {
+export function getFile(pool, file_uuid = "", other_join = ["", ""]) {
   let conn;
   let parameters = [];
-  let query_from = ["File"];
+  const [join_table, join_uuid] = other_join;
   let query = `SELECT HEX(File.uuid) as uuid, 
-                 File.filename as filename, 
-                 File.description as description, 
-                 File.option as option, 
-                 File.data as data
-                 From {0}  
-                 WHERE 1=1 `;
+                      File.filename as filename, 
+                      File.description as description, 
+                      File.option as option, 
+                      CONVERT(File.data USING utf8) as data
+                FROM File `;
   if (file_uuid) {
-    query += ` AND File.uuid = UNHEX(?) `;
+    query += " WHERE File.uuid = UNHEX(?) ";
     parameters.push(file_uuid);
   }
-  if (other_uuid) {
-    query_from.push(
-      "ObjectFile",
-      "Object",
-      "PlaceFile",
-      "Place",
-      "MapFile",
-      "Map",
-      "RoomFile",
-      "Room"
-    );
-    const sub_query = `
-        (ObjectFile.FileId = File.id AND Object.id = ObjectFile.ObjectId AND Object.uuid = UNHEX(?) ) OR 
-        (PlaceFile.FileId = File.id AND Place.id = PlaceFile.PlaceId AND Place.uuid = UNHEX(?) ) OR 
-        (MapFile.FileId = File.id AND Map.id = MapFile.MapId AND Map.uuid = UNHEX(?) ) OR 
-        (RoomFile.FileId = File.id AND Room.id = RoomFile.RoomId AND Room.uuid = UNHEX(?) ) 
-    `;
-    query += `AND (${sub_query})`;
-    for (let i = 0; i < 4; i++) {
-      parameters.push(other_uuid);
+  if (join_table && join_uuid) {
+    const formatted_join_table = join_table.toLocaleLowerCase();
+    switch (formatted_join_table) {
+      case "object":
+        query += ` INNER JOIN ObjectFile ON File.id = ObjectFile.FileId `;
+        query += ` INNER JOIN Object ON Object.id = ObjectFile.ObjectId AND Object.uuid = UNHEX(?) `;
+        parameters.push(join_uuid);
+        break;
+      case "place":
+        query += ` INNER JOIN PlaceFile ON File.id = PlaceFile.FileId `;
+        query += ` INNER JOIN Place ON Place.id = PlaceFile.PlaceId AND Place.uuid = UNHEX(?) `;
+        parameters.push(join_uuid);
+        break;
+      case "room":
+        query += ` INNER JOIN RoomFile ON File.id = RoomFile.FileId `;
+        query += ` INNER JOIN Room ON Room.id = RoomFile.RoomId AND Room.uuid = UNHEX(?) `;
+        parameters.push(join_uuid);
+        break;
+      case "map":
+        query += ` INNER JOIN MapFile ON File.id = MapFile.FileId `;
+        query += ` INNER JOIN Map ON Map.id = MapFile.MapId AND Map.uuid = UNHEX(?) `;
+        parameters.push(join_uuid);
+        break;
+      default:
+        return Promise.reject(
+          new Error(`Table '${join_table}' doesn't exist !`)
+        );
     }
   }
-  query = query.replace("{0}", query_from.join(", "));
   return pool
     .getConnection()
     .then((connexion) => {
@@ -72,9 +77,10 @@ export const File = (app, pool) => {
         handlerError(err, req, res, next);
       });
   });
-  app.get("/api/file/element/:uuid", async function (req, res, next) {
+  app.get("/api/file/:element/:uuid", async function (req, res, next) {
     const uuid = req.params.uuid;
-    getFile(pool, "", uuid)
+    const element = req.params.element;
+    getFile(pool, "", [element, uuid])
       .then((result) => {
         handlerSuccess(result, req, res, next);
       })
