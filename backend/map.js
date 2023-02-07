49049c40554263bfe2fc6ad75881be7be62f0ee4
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2023-02-07 11:35:27 +0100

    changement nom .js -> .mjs

--- backend/map.js
+++ backend/map.js
@@ -1,5 +1,6 @@
 import { handlerError, handlerSuccess } from "./handler.js";
 import { getPlace } from "./place.js";
+import MapObject from "../objects/map.mjs";
 
 export function getMap(pool, map_uuid = "", full = false) {
   let query = "SELECT name, HEX(uuid) as uuid FROM Map";
@@ -30,6 +31,28 @@ export function getMap(pool, map_uuid = "", full = false) {
     });
 }
 
+export function postMap(pool, { name, uuid }) {
+  if (!name) {
+    throw new Error(`variable "name" maquant !`);
+  }
+  let query = `INSERT INTO Map (name) VALUES (?) `;
+  let parameters = [name];
+  if (uuid) {
+    query = `UPDATE Map SET Map.name = ? WHERE Map.uuid = UNHEX(?) `;
+    let parameters = [name, uuid];
+  }
+  return pool
+    .getConnection()
+    .then((connexion) => {
+      conn = connexion;
+      return conn.query(query, parameters);
+    })
+    .then((result) => result)
+    .finally(() => {
+      if (conn) conn.end();
+    });
+}
+
 export const Map = (app, pool) => {
   app.get("/api/map", async function (req, res, next) {
     const ask_full = req.query.full !== undefined;
@@ -45,6 +68,16 @@ export const Map = (app, pool) => {
     const uuid = req.params.uuid;
     const ask_full = req.query.full !== undefined;
     getMap(pool, uuid, ask_full)
+      .then((result) => {
+        handlerSuccess(result, req, res, next);
+      })
+      .catch((err) => {
+        handlerError(err, req, res, next);
+      });
+  });
+  app.post("/api/map", async function (req, res, next) {
+    const data = { ...MapObject, ...req.body };
+    postMap(pool, data)
       .then((result) => {
         handlerSuccess(result, req, res, next);
       })
