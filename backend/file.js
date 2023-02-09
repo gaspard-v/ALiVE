a601eea7e7c811aa8e578a77bc16d58039e93d8f
gaspard.vandecasteele <gaspard.vandecasteele@gmail.com>
2023-02-09 14:16:37 +0100

    revert commit fix base64
    cause: base64 is already working well

--- backend/file.js
+++ backend/file.js
@@ -8,7 +8,7 @@ export function getFile(pool, file_uuid = "", other_join = ["", ""]) {
                       File.filename as filename, 
                       File.description as description, 
                       File.option as option, 
-                      TO_BASE64(File.data) as data
+                      CONVERT(File.data USING utf8) as data
                 FROM File `;
   if (file_uuid) {
     query += " WHERE File.uuid = UNHEX(?) ";
