c089cc879a1bc4c1c790d5564223dcb940736352
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2023-02-20 22:53:47 +0100

    WIP scroller

--- frontend/phaser/src/objects/ItemsContainer.js
+++ frontend/phaser/src/objects/ItemsContainer.js
@@ -1,35 +1,36 @@
 import ItemSelect from "./ItemSelect";
 
 export class ItemContainer{
-    constructor(x,y,objectData,scene){
+    constructor(x,y,objectData,scene,mask,xRectTopLeft,yRectTopLeft){
 
-        const borderRadius = 20;
-       
-        // Create a sprite and add it to the scene
-        const sprite = scene.add.sprite(0, 0, 'itemGroup');
-        const xRect = x - sprite.width/2;
-        const yRect = y - sprite.height/2;
-
-        // Create a graphics object for the mask
-        const maskGraphics = scene.make.graphics();
-
-        // Set the fill style and draw a rounded rectangle on the graphics object
-        maskGraphics.fillStyle(0xffffff, 1);
-        maskGraphics.fillRoundedRect(xRect, yRect, sprite.width, sprite.height, borderRadius);
-
-        // Create a mask from the graphics object
-        const mask = maskGraphics.createGeometryMask();
-
-        // Set the mask on the sprite
-        sprite.setMask(mask);
-        const frame = scene.add.sprite(0,0,'itemFrame');
-
-        const container = scene.add.container(x,y,[sprite])
-        objectData.map((object)=>{
-            const imageObject = scene.add.sprite(0,0,"image_"+object.object)
-            container.add(imageObject)
-            
-        })
+        const container = scene.add.container(x,y)
         
+        let xObject = 100;
+        let yObject = -250; 
+    
+        objectData.map((object,index)=>{
+            const frame = scene.add.sprite(xObject,yObject,'itemFrame');
+            const padding = frame.height/4;
+
+            const imageObject = scene.add.sprite(xObject,yObject,"image_"+object.object)
+            if(index%2 == 0){
+                xObject = xObject*(-1);
+            }
+            else{
+                yObject = yObject + frame.height + padding;
+                
+            }
+            
+            frame.setPosition(xObject,yObject);
+            imageObject.setScale(frame.width/imageObject.width,frame.height/imageObject.height)
+            container.add(imageObject);
+            container.add(frame);
+
+            
+            // Le masque permet de couper les objets 
+            container.setMask(mask);
+            
+        });   
     }
+
 }
