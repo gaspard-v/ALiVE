8e7fe08fcdd31bf749e90d72b7338403ff08aea2
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2023-02-20 22:53:47 +0100

    WIP scroller

--- frontend/phaser/src/scenes/Reflection.js
+++ frontend/phaser/src/scenes/Reflection.js
@@ -1,6 +1,5 @@
 import * as Phaser from 'phaser';
 import { ItemContainer } from '../objects/ItemsContainer';
-import ItemSelect from '../objects/ItemSelect';
 
 export default class Reflection extends Phaser.Scene{
     constructor(handle){
@@ -8,16 +7,39 @@ export default class Reflection extends Phaser.Scene{
         this.inventory = [];
         this.itemsSelection = [];
     }
+    preload(){
+    }
+   
     create(){
+        const borderRadius = 20;
+        const x = 480;
+        const y = 540;
         this.getObjectData();
-    
-        const items = new ItemContainer(480,540,this.itemsSelection,this);
+
+        // Create a sprite and add it to the scene
+        const sprite = this.add.sprite(x, y, 'itemGroup');
+        const xRectTopLeft = x - sprite.width/2;
+        const yRectTopLeft = y - sprite.height/2;
+
+        // Create a graphics object for the mask
+        const maskGraphics = this.make.graphics()
+                    .fillStyle(0xffffff, 1)
+                    .fillRoundedRect(xRectTopLeft, yRectTopLeft, sprite.width, sprite.height, borderRadius);
         
+        const mask = maskGraphics.createGeometryMask();
         
+        // Set the mask on the sprite
+        sprite.setMask(mask);
+
+        const items = new ItemContainer(x,y,this.itemsSelection,this,mask,xRectTopLeft,yRectTopLeft);           
+       
         
     }
+  
 
     getObjectData(){
+
+        // If you want to change the data, you get it here 
         const mapData = this.cache.json.get('mapData');
         const placeData = mapData.content[0].places;
         
@@ -33,4 +55,5 @@ export default class Reflection extends Phaser.Scene{
     
         })
     }
+    
 }
