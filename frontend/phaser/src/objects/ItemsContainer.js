1689d71ba467ab7c57f79c41e2913b2f1b5ce7e0
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2023-02-23 15:03:12 +0100

    Scroll Done

--- frontend/phaser/src/objects/ItemsContainer.js
+++ frontend/phaser/src/objects/ItemsContainer.js
@@ -4,6 +4,7 @@ export class ItemContainer{
     constructor(x,y,objectData,scene,mask,xRectTopLeft,yRectTopLeft){
 
         const container = scene.add.container(x,y)
+        container.setName('dayInventoryContainer')
         
         let xObject = 100;
         let yObject = -250; 
@@ -11,26 +12,28 @@ export class ItemContainer{
         objectData.map((object,index)=>{
             const frame = scene.add.sprite(xObject,yObject,'itemFrame');
             const padding = frame.height/4;
-
             const imageObject = scene.add.sprite(xObject,yObject,"image_"+object.object)
+
+            // Creates the Objects by pair  
+
             if(index%2 == 0){
                 xObject = xObject*(-1);
             }
             else{
                 yObject = yObject + frame.height + padding;
-                
             }
-            
             frame.setPosition(xObject,yObject);
             imageObject.setScale(frame.width/imageObject.width,frame.height/imageObject.height)
+
+            // Add the objects to the container
             container.add(imageObject);
             container.add(frame);
-
             
-            // Le masque permet de couper les objets 
+            
             container.setMask(mask);
             
         });   
+        container.height= yObject;
     }
 
 }
