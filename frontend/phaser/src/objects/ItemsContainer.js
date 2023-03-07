d4faaf07ab6c6e16bc8236a1ce1cd0e466276053
maxence.palkowski1 <maxence.palkowski1@gmail.com>
2023-03-07 14:41:05 +0100

    WIP Selection

--- frontend/phaser/src/objects/ItemsContainer.js
+++ frontend/phaser/src/objects/ItemsContainer.js
@@ -9,9 +9,7 @@ export class ItemContainer{
         const container = scene.add.container(x,y)
         container.setName('dayInventoryContainer')
 
-        let xObject = -100;
-        let yObject = -250;
-
+        
         scene.add.text(
             xRectTopLeft + 80 - this.title.toString().length/2,
             yRectTopLeft - 50,
@@ -22,47 +20,12 @@ export class ItemContainer{
                 color: '#ffffff'
             }
         );
-
-        this.objectData.forEach((object,index)=>{
-            const frame = scene.add.sprite(xObject,yObject,'itemFrame');
-            const padding = frame.height/4;
-
-
-            if(index%2 === 0 && index!==0){
-                yObject = yObject + frame.height + padding;
-            }
-
-            if (object.object) {
-                const imageObject = scene.add.sprite(xObject,yObject,"image_"+object.object);
-                imageObject.setPosition(xObject,yObject);
-                imageObject.setScale(frame.width/imageObject?.width,frame.height/imageObject?.height);
-                imageObject.setInteractive();
-                imageObject.on('pointerdown', function () {
-                    this.scene.events.emit("addObject", {title, info: object});
-                });
-                container.add(imageObject);
-            }
-
-            frame.setPosition(xObject,yObject);
-            container.add(frame);
-            xObject = xObject*(-1);
-
-            // Le masque permet de couper les objets
-
-        });
+        
         container.setMask(mask);
-            
-    
-
+        
     }
 
     setObjectData(objectData) {
         this.objectData = objectData;
     }
-
-    update() {
-        this.objectData = data;
-        console.log('update : ', this.objectData);
-    }
-
 }
