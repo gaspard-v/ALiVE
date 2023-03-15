7b3773f564b504dca9b94ad3cc3e7b22669ef907
Gaspard VANDECASTEELE <gaspard.vandecasteele@student.yncrea.fr>
2023-03-15 22:09:04 +0100

    modification change timer time

--- frontend/phaser/src/scenes/Places.js
+++ frontend/phaser/src/scenes/Places.js
@@ -1,46 +1,45 @@
-import Rooms from "./Rooms";
-import {isReflectionDelayOver} from "./ReflectionButton.js"
-import ReflectionButton from "./ReflectionButton"
-import { Button } from "../gameObjects/mainMenu/Button";
-import axios from "axios";
+import Rooms from './Rooms';
+import { isReflectionDelayOver } from './ReflectionButton.js';
+import ReflectionButton from './ReflectionButton';
+import { Button } from '../gameObjects/mainMenu/Button';
+import axios from 'axios';
 
-export default class Places extends Phaser.Scene{
+export default class Places extends Phaser.Scene {
     axiosExperiment;
-    constructor(handle,roomsData){
+    constructor(handle, roomsData) {
         super(handle);
         this.rooms = roomsData;
-        isReflectionDelayOver.bool = false
-        this.launch = "";
+        isReflectionDelayOver.bool = false;
+        this.launch = '';
     }
-    preload(){
+    preload() {}
 
-    }
-    
-    
     async create() {
         // Timer for the scene
 
-        let timer = this.time.addEvent({delay: 1000, callback: this.onTimerEnd, callbackScope: this});
-
+        let timer = this.time.addEvent({
+            delay: 215000, // 3min 25s
+            callback: this.onTimerEnd,
+            callbackScope: this,
+        });
 
         // Find an other solution to this double map
         this.rooms
-            .filter(({uuid}) => !this.scene.isActive(uuid))
-            .forEach(({uuid, objects, doors, roomFile}) => {
+            .filter(({ uuid }) => !this.scene.isActive(uuid))
+            .forEach(({ uuid, objects, doors, roomFile }) => {
                 const room = new Rooms(uuid, objects, doors, roomFile);
                 this.scene.add(uuid, room);
-                this.launch = uuid
+                this.launch = uuid;
             });
-            this.scene.launch(this.launch)
+        this.scene.launch(this.launch);
     }
 
-    onTimerEnd()
-    {
-        isReflectionDelayOver.bool = true
-        if(!this.scene.isActive('reflectionButton')){
+    onTimerEnd() {
+        isReflectionDelayOver.bool = true;
+        if (!this.scene.isActive('reflectionButton')) {
             const reflectionButton = new ReflectionButton('reflectionButton');
-            this.scene.add('reflectionButton',reflectionButton,true);
+            this.scene.add('reflectionButton', reflectionButton, true);
         }
-        this.scene.bringToTop('reflectionButton')
+        this.scene.bringToTop('reflectionButton');
     }
 }
--- frontend/phaser/src/scenes/Reflection.js
+++ frontend/phaser/src/scenes/Reflection.js
@@ -3,92 +3,103 @@ import { ItemContainer } from '../objects/ItemsContainer';
 import axios from 'axios';
 import { FinalPrompt } from './FinalPrompt';
 
-const DISCOVERED_OBJECTS = "Objets découverts";
-const BACKPACK = "Mon sac à dos";
+const DISCOVERED_OBJECTS = 'Objets découverts';
+const BACKPACK = 'Mon sac à dos';
 
-
-export default class Reflection extends Phaser.Scene{
+export default class Reflection extends Phaser.Scene {
     items;
     itemsInventory;
     container;
-    
-    
-    constructor(handle,data){
+
+    constructor(handle, data) {
         super(handle);
         this.inventory = [{}, {}, {}];
         this.itemsSelection = data;
         this.scroller;
-    }   
-   create(){
-    
+    }
+    create() {
         const x = 480;
         const y = 540;
 
-        const spriteInventory = this.add.sprite(3*x, y, 'itemGroup');
-        const xInventoryRectTopLeft = 3*x - spriteInventory.width/2;
-        const yInventoryRectTopLeft = y - spriteInventory.height/2;
-        
+        const spriteInventory = this.add.sprite(3 * x, y, 'itemGroup');
+        const xInventoryRectTopLeft = 3 * x - spriteInventory.width / 2;
+        const yInventoryRectTopLeft = y - spriteInventory.height / 2;
+
         const slidingDeceleration = 5000;
         const backDeceleration = 2000;
         var bottomBound = 0;
         const borderRadius = 20;
 
-
         // Create a sprite and add it to the scene
         const inventoryBackground = this.add.sprite(x, y, 'itemGroup');
-        const xRectTopLeft = x - inventoryBackground.width/2;
-        var yRectTopLeft = y - inventoryBackground.height/2;
+        const xRectTopLeft = x - inventoryBackground.width / 2;
+        var yRectTopLeft = y - inventoryBackground.height / 2;
         var topBound = yRectTopLeft;
-        
+
         // Create a graphics object for the mask
-        const maskGraphics = this.make.graphics()
-        .fillStyle(0xffffff, 1)
-        .fillRoundedRect(xRectTopLeft, yRectTopLeft, inventoryBackground.width, inventoryBackground.height, borderRadius);
-        
-        const mask = maskGraphics.createGeometryMask();
-        
-        // Set the mask on the sprite
-        const inventoryMaskGraphics = this.make.graphics()
+        const maskGraphics = this.make
+            .graphics()
             .fillStyle(0xffffff, 1)
-            .fillRoundedRect(xInventoryRectTopLeft, yInventoryRectTopLeft, spriteInventory.width, spriteInventory.height, borderRadius);
+            .fillRoundedRect(
+                xRectTopLeft,
+                yRectTopLeft,
+                inventoryBackground.width,
+                inventoryBackground.height,
+                borderRadius
+            );
+
+        const mask = maskGraphics.createGeometryMask();
+
+        // Set the mask on the sprite
+        const inventoryMaskGraphics = this.make
+            .graphics()
+            .fillStyle(0xffffff, 1)
+            .fillRoundedRect(
+                xInventoryRectTopLeft,
+                yInventoryRectTopLeft,
+                spriteInventory.width,
+                spriteInventory.height,
+                borderRadius
+            );
         const inventoryMask = inventoryMaskGraphics.createGeometryMask();
         inventoryBackground.setMask(mask);
 
         // Creates the content from the item selection
-        this.items = new ItemContainer(x,y,{
-            title: DISCOVERED_OBJECTS,
-            data: this.itemsSelection
-        },this,mask,xRectTopLeft,yRectTopLeft);
-        
+        this.items = new ItemContainer(
+            x,
+            y,
+            {
+                title: DISCOVERED_OBJECTS,
+                data: this.itemsSelection,
+            },
+            this,
+            mask,
+            xRectTopLeft,
+            yRectTopLeft
+        );
+
         // Get the created container
         const container = this.children.getByName('dayInventoryContainer');
-        
-        
-        
-        if (container.height > inventoryBackground.height){
+
+        if (container.height > inventoryBackground.height) {
             bottomBound = y - container.height + inventoryBackground.height;
-        } 
-        else{
+        } else {
             bottomBound = y;
         }
 
-        // Scroller is a plugin using the inventoryBackground sprite as a 
-        
-    
-        this.scroller = this.plugins.get('rexScroller').add(inventoryBackground, {
-            bounds: [
-                bottomBound,
-                topBound
-            ],
-            value: bottomBound,
-            slidingDeceleration: slidingDeceleration,
-            backDeceleration: backDeceleration,
-            valuechangeCallback: function (value) {
-                container.y = value;
-            }
+        // Scroller is a plugin using the inventoryBackground sprite as a
 
-        });
-
+        this.scroller = this.plugins
+            .get('rexScroller')
+            .add(inventoryBackground, {
+                bounds: [bottomBound, topBound],
+                value: bottomBound,
+                slidingDeceleration: slidingDeceleration,
+                backDeceleration: backDeceleration,
+                valuechangeCallback: function (value) {
+                    container.y = value;
+                },
+            });
 
         /// Inventory part
         // Create a sprite and it to the scene
@@ -100,108 +111,122 @@ export default class Reflection extends Phaser.Scene{
 
         const objectData = {
             title: BACKPACK,
-            data: this.inventory
-        }
+            data: this.inventory,
+        };
 
-        this.itemsInventory = new ItemContainer(x+960, y, objectData, this, inventoryMask, xInventoryRectTopLeft, yInventoryRectTopLeft);
-        
+        this.itemsInventory = new ItemContainer(
+            x + 960,
+            y,
+            objectData,
+            this,
+            inventoryMask,
+            xInventoryRectTopLeft,
+            yInventoryRectTopLeft
+        );
 
-        this.events.on('addObject',  ({title, info}) => {
+        this.events.on('addObject', ({ title, info }) => {
             if (title === DISCOVERED_OBJECTS) {
                 this.addToInventory(info);
             } else {
                 this.addToSelection(info); // retirer un objet de la liste du sac à dos
             }
         });
-        
-        let timer = this.time.addEvent({delay: 5000, callback: this.onTimerEnd, callbackScope: this})
+
+        let timer = this.time.addEvent({
+            delay: 70000, //1min 10s
+            callback: this.onTimerEnd,
+            callbackScope: this,
+        });
     }
-    onTimerEnd(){
-        const end = new FinalPrompt('finalScene',this.inventory);
-        this.scene.add('finalScene',end);
+    onTimerEnd() {
+        const end = new FinalPrompt('finalScene', this.inventory);
+        this.scene.add('finalScene', end);
         this.scene.start(end);
-        
     }
 
     addToInventory(object) {
         //this.stateValues('add to inventory : ');
-        for (let i = 0; i < this.inventory.length; i++){
-            if(!this.inventory[i].object){
+        for (let i = 0; i < this.inventory.length; i++) {
+            if (!this.inventory[i].object) {
                 this.inventory[i] = object;
-               switch(i){
-                        case 0:
-                             object.xObject = -100;
-                             object.yObject = -250;
-                             break;
-                        case 1:
-                             object.xObject = 100;
-                             object.yObject = -250;
-                             break
-                        case 2:
-                            object.xObject = -100;
-                            object.yObject = -100;
-                            break
-                    }
-                    this.addToView(this.itemsInventory,object);
-                    this.delFromView(this.items,object)
-                    break
+                switch (i) {
+                    case 0:
+                        object.xObject = -100;
+                        object.yObject = -250;
+                        break;
+                    case 1:
+                        object.xObject = 100;
+                        object.yObject = -250;
+                        break;
+                    case 2:
+                        object.xObject = -100;
+                        object.yObject = -100;
+                        break;
                 }
+                this.addToView(this.itemsInventory, object);
+                this.delFromView(this.items, object);
+                break;
             }
-            this.removeFromSelection(object);
         }
-        
-        addToSelection(object) {
-        for(let i = 0; i< this.items.objectData.length; i++){    
-            if(!this.items.objectData[i].object){
+        this.removeFromSelection(object);
+    }
+
+    addToSelection(object) {
+        for (let i = 0; i < this.items.objectData.length; i++) {
+            if (!this.items.objectData[i].object) {
                 this.items.objectData[i] = object;
-                const frame = this.items.container.getByName("frame_"+object.index)
+                const frame = this.items.container.getByName(
+                    'frame_' + object.index
+                );
                 object.xObject = frame.x;
                 object.yObject = frame.y;
-                break
+                break;
             }
         }
-        this.delFromView(this.itemsInventory,object);
+        this.delFromView(this.itemsInventory, object);
         this.removeFromInventory(object);
-        this.addToView(this.items,object);
+        this.addToView(this.items, object);
         // this.stateValues('add to selection : ');
     }
- 
+
     removeFromInventory(object) {
         for (let i = 0; i < this.inventory.length; i++) {
             if (this.inventory[i].object === object.object) {
-                this.inventory[i] = {"index":i};
+                this.inventory[i] = { index: i };
                 break;
             }
         }
     }
- 
+
     removeFromSelection(object) {
         for (let i = 0; i < this.itemsSelection.length; i++) {
             if (this.itemsSelection[i].object === object.object) {
-                this.itemsSelection[i] = {"index":i};
+                this.itemsSelection[i] = { index: i };
                 break;
             }
         }
     }
- 
-    addToView(items,object){                
+
+    addToView(items, object) {
         // ajout d'un objet dans l'inventaire (sac à dos)
-        const sprite = this.add.sprite(object.xObject,object.yObject,"image_"+object.object);
-        sprite.setName("image_"+object.object)
-                .setScale((120/sprite?.height)*0.7)
+        const sprite = this.add.sprite(
+            object.xObject,
+            object.yObject,
+            'image_' + object.object
+        );
+        sprite
+            .setName('image_' + object.object)
+            .setScale((120 / sprite?.height) * 0.7);
 
         const title = items.title;
-        sprite.setInteractive()
-                .on('pointerdown', function () {
-                    this.scene.events.emit("addObject", {title, info: object});
-                });
+        sprite.setInteractive().on('pointerdown', function () {
+            this.scene.events.emit('addObject', { title, info: object });
+        });
         items.container.add(sprite);
+    }
 
+    delFromView(items, object) {
+        const sprite = items.container.getByName('image_' + object.object);
+        sprite.destroy();
     }
- 
-    delFromView(items,object){
-        const sprite = items.container.getByName("image_"+object.object)
-        sprite.destroy()        
-    }
- 
 }
